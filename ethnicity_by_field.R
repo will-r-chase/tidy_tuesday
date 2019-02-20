ad2767d8559d28146c95836890aa73b23e6a7ce5
William Chase <wchase14@gmail.com>
2019-02-19 21:16:37 -0500

    add ethnicity and gender by field

---
+++ ethnicity_by_field.R
@@ -0,0 +1,42 @@
+#have to load fonts before ggplot
+extrafont::loadfonts(device="win")
+
+library(readxl)
+library(tidyverse)
+#devtools::install_github("gadenbuie/ggpomological")
+library(ggpomological)
+
+data <- read_xlsx("data/ethnicity_by_field.xlsx", skip = 3) %>%
+  select(field, ethnicity, phds = "2017") %>%
+  filter(ethnicity != "Not Hispanic or Latino")
+
+totals <-
+  data %>%
+  filter(ethnicity == "total") %>%
+  select(field, total = phds)
+
+data_percent <- 
+  data %>%
+  right_join(., totals, by = "field") %>%
+  mutate(percent = round(phds/total*100, 2)) %>%
+  filter(ethnicity != "total") %>%
+  mutate(ethnicity = ifelse(ethnicity == "Other race or race not reported", "Other or not reported", ethnicity),
+         field = case_when(
+           field == "Mathematics and computer sciences" ~ "Math and computer sciences",
+           field == "Physical sciences and earth sciences" ~ "Physical and earth sciences",
+           TRUE ~ field
+         ))
+
+ggplot(data_percent, aes(x = field, y = percent, fill = ethnicity)) +
+  #stacked bar
+  geom_bar(stat = "identity") +
+  #design stuff
+  scale_fill_manual("Ethnicity", values = c("#828585", "#4f5157", "#fd8f24", "#c03728", "#f5c04a", "#e68c7c", "#6f5438", "#919c4c")) +
+  coord_flip() +
+  theme_pomological(base_family="Lato", base_size = 14) +
+  theme(plot.subtitle = element_text(face = "italic", size = 14), 
+        plot.title = element_text(size = 18),
+        plot.caption = element_text(face = "italic", size = 8, vjust = 0.5, hjust = 1)) +
+  labs(x = "Year", y = "Percent", title = "PhDs awarded by ethnicity", subtitle = "Grad school is very white, but changing slowly", caption = "Graphic: @W_R_Chase\nData: NCSES")
+
+ggsave("ethnicity_by_field.png", device = "png", type = "cairo", height = 6, width = 6.5)
---
+++ gender_gap_field.R
@@ -0,0 +1,41 @@
+#have to load fonts before ggplot
+extrafont::loadfonts(device="win")
+
+library(readxl)
+library(tidyverse)
+#devtools::install_github("gadenbuie/ggpomological")
+library(ggpomological)
+library(ggalt)
+
+#read in data
+data <- read_xlsx("data/sex_by_field.xlsx", skip = 3) %>%
+  filter(field != "All fields")
+
+data_change <- 
+  data %>%
+  gather(key = year, value = percent, -field, -sex) %>%
+  spread(key = sex, value = percent) %>%
+  mutate(field = case_when(
+    field == "Mathematics and computer sciences" ~ "Math and computer sciences",
+    field == "Physical sciences and earth sciences" ~ "Physical and earth sciences",
+    TRUE ~ field
+  )) %>%
+  group_by(field, year) %>%
+  mutate(change = round(Male - Female, 2), label = ifelse(change > 0, paste0("+", change, "%"), paste0(change, "%")))
+
+data_change$field <- fct_relevel(data_change$field, "Education", "Psychology and social sciences", "Life sciences",
+                                 "Other", "Humanities and arts", "Physical and earth sciences", 
+                                 "Math and computer sciences", "Engineering")
+                                
+ggplot(data_change, aes(x = year, y = field, fill = change))+
+  geom_tile(color = "#FFFEEA") +
+  scale_fill_gradient2(low = "#919c4c", high = "#f5c04a", mid = "white", 
+                      space = "Lab", 
+                       name="Gender imbalance (%)", guide = FALSE) +
+  theme_pomological(base_family="Lato", base_size = 14) +
+  geom_text(aes(x = year, y = field, label = label), color = "black", size = 4) +
+  theme(plot.subtitle = element_text(face = "italic", size = 14), 
+        plot.title = element_text(size = 18),
+        plot.caption = element_text(face = "italic", size = 8, vjust = 0.5, hjust = 1)) +
+  labs(x = "Year", y = "Field", title = "Percent of PhDs awarded to men compared to women", subtitle = "I don't know what I expected...", caption = "Graphic: @W_R_Chase\nData: NCSES") +
+  coord_fixed()
